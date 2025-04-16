de2e473a3032c8116c904ac6c6a9cef0f2074965
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-04-16 10:01:51 -0400

    add comments to parser

--- src/parser.rs
+++ src/parser.rs
@@ -1,39 +1,69 @@
-use crate::{
-    lexer::{Token, TT}, AddFields, ConstantFields, DivFields, Instr, MulFields, ReturnFields, StartFields, SubFields, Type
-};
-use std::{io, sync::Arc};
+use crate::{lexer::{Token, TT}, AddFields, ConstantFields, DivFields, Instr, MulFields, Scope, ReturnFields, StartFields, SubFields, Type};
+use std::{io, sync::{Arc, LazyLock, Mutex}};
 
-pub static START: Instr =  Instr::Start(StartFields { id: 0, typ: Type::Bot });
+// NB1. each function in the parser will parse in two ways
+//        a. conditionally (SUM/OR): match tokens(first, rest) first.typ { TT::Foo => {}, TT::Bar => {}, TT::Baz => {} }
+//        b. assertively (PROD/AND): require(tokens, TT:Foo), eat(tokens, TT:Bar), eat(tokens, TT:Baz)
 
+// NB2. the parser is composed of pure functions less the start instruction and
+//      scope/nv instruction so the token stream (r) is threaded throughout
+
+// NB3. generally speaking there are three variants for intermediate representation
+//        a. tree ("AST")
+//           -> precedence is represented via tree's hierarchy.
+//        b. two-tiered (nested) graph of basic blocks of instructions. ("CFG+BB")
+//           -> control flow is represented via graph's edges
+//        c. single-tiered (flat) graph of instructions ("SoN")
+//           -> control flow AND data flow is represented via graph's edges
+
+//      picotile parses the concrete syntax into (c) because TODO. see optimizer
+//      for more details. this means that the total ordering of straightline
+//      code (vec<list>) is relaxed to a partial order of a graph
+
+pub static START: Instr = Instr::Start(StartFields { id: 0, typ: Type::Bot });
+static SCOPE: LazyLock<Mutex<Scope>> = LazyLock::new(|| Mutex::new(Scope::new()));
 
 pub fn parse_prg(tokens: &[Token]) -> Result<Instr, io::Error> {
     let r = tokens;
-    let (_, r) = mtch(r, TT::KeywordInt)?;
-    let (_, r) = mtch(r, TT::Alias)?;
-    let (_, r) = mtch(r, TT::PuncLeftParen)?;
-    let (_, r) = mtch(r, TT::PuncRightParen)?;
+    let (_, r) = require(r, TT::KeywordInt)?;
+    let (_, r) = require(r, TT::Alias)?;
+    let (_, r) = require(r, TT::PuncLeftParen)?;
+    let (_, r) = require(r, TT::PuncRightParen)?;
 
-    let (_, r) = mtch(r, TT::PuncLeftBrace)?;
+    let (_, r) = require(r, TT::PuncLeftBrace)?;
     let (stmt, r) = parse_stmt(r)?;
-    let (_, r) = mtch(r, TT::PuncRightBrace)?;
+    let (_, r) = require(r, TT::PuncRightBrace)?;
 
-    if r.is_empty() {
-        Ok(stmt)
-    } else {
-        Err(io::Error::new(
-            io::ErrorKind::Other,
-            format!("expected empty token stream, got {:?}", r),
-        ))
-    }
+    if r.is_empty() { Ok(stmt) } else { Err(io::Error::new(io::ErrorKind::Other,format!("expected empty token stream, {:?}", r)))}
+}
+
+fn parse_block(tokens: &[Token]) -> Result<(Instr, &[Token]), io::Error> {
+    SCOPE.lock().unwrap().push();
+    let (mut foo, mut r) = (None, tokens);
+    while let Ok((bar, _r)) = parse_stmt(r) {
+        foo = Some(bar);
+        r = _r;
+    };
+    SCOPE.lock().unwrap().pop();
+    Ok((foo.unwrap(), r))
 }
 
 fn parse_stmt(tokens: &[Token]) -> Result<(Instr, &[Token]), io::Error> {
     match tokens {
         [] => Err(io::Error::new(io::ErrorKind::Other, "expected: {:?} got an empty token stream")),
         [f, r @ ..] => match f.typ {
+            TT::KeywordInt => {
+                let (alias, r) = require(r, TT::Alias)?;
+                let (_, r) = require(r, TT::Equals)?;
+                let (expr, r) = parse_expr(r)?;
+                let (_, r) = require(r, TT::PuncSemiColon)?;
+                
+                // Ok((SStmt::Asnmt(a), r))
+                todo!()
+            },
             TT::KeywordRet => {
                 let (expr, r) = parse_term( r)?;
-                let (_, r) = mtch(r, TT::PuncSemiColon)?;
+                let (_, r) = require(r, TT::PuncSemiColon)?;
                 let retinstr = Instr::Return(ReturnFields::new(Arc::new(START.clone()), expr));
                 Ok((retinstr, r))
             }
@@ -43,6 +73,10 @@ fn parse_stmt(tokens: &[Token]) -> Result<(Instr, &[Token]), io::Error> {
             )),
         },
     }
+}
+
+fn parse_expr(tokens: &[Token]) -> Result<(Instr, &[Token]), io::Error> {
+    parse_term(tokens)
 }
 
 fn parse_term(tokens: &[Token]) -> Result<(Instr, &[Token]), io::Error> {
@@ -106,7 +140,7 @@ fn parse_atom(tokens: &[Token]) -> Result<(Instr, &[Token]), io::Error> {
     }
 }
 
-fn mtch(tokens: &[Token], tt: TT) -> Result<(&Token, &[Token]), io::Error> {
+fn require(tokens: &[Token], tt: TT) -> Result<(&Token, &[Token]), io::Error> {
     match tokens {
         [] => Err(io::Error::new(
             io::ErrorKind::Other,
@@ -268,5 +302,27 @@ mod test_arith {
             },
         )
         "###);
+    }
+}
+
+
+#[cfg(test)]
+mod test_bindings {
+    use crate::lexer;
+    use std::fs;
+
+    const TEST_DIR: &str = "tests/fixtures/snap/shared/bindings";
+
+    #[test]
+    fn assignment() {
+        let chars = fs::read(format!("{TEST_DIR}/assignment.c"))
+            .expect("file dne")
+            .iter()
+            .map(|b| *b as char)
+            .collect::<Vec<_>>();
+
+        let tokens = lexer::lex(&chars).unwrap();
+        let graph = super::parse_prg(&tokens).unwrap();
+        insta::assert_debug_snapshot!(graph, @r"");
     }
 }
