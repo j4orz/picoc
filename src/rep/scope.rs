e2b016657211a9f945fa51f7ea4dbd92db3bd553
jeffzh4ng <zhang.jeffreyd@gmail.com>
2025-04-19 08:43:46 -0400

    create scope instr

--- src/rep/scope.rs
+++ src/rep/scope.rs
@@ -1,47 +1,88 @@
-use std::{collections::{HashMap, VecDeque}, fmt::Debug, rc::Rc, sync::Arc};
+use std::{cell::RefCell, collections::{HashMap, VecDeque}, fmt::Debug, rc::{Rc, Weak}};
 use thiserror::Error;
-
-use super::{Instr, TypeKind};
+use super::{Instr, InstrKind, TypeKind};
 
 #[derive(Error, Debug)]
-pub enum FooError {
-    #[error("broadcast mismatch")]
-    BroadcastMismatch,
+pub enum ScopeError {
     #[error("double define")]
     DoubleDefine,
     #[error("unknown scope error")]
     Unknown,
 }
 
+// NB1: the program scope is a stack of nvs which are themselves maps from
+//      aliases to usizes which are indices into its inputs. every instruction
+//      node is dead on creation unless aliased by the scope node. once an
+//      expression is aliased, it lives until the nv corresponding to the
+//      lexical scope is popped by the caller and dropped by rust.
+// NB2: each map in the stack from last to first corresponds to increasingly
+//      nested lexical scopes. the language we are compiling for is sequential
+//      not parallel so there can only be one active map for index i at any
+//      given time. for example, in the snippet below, only one map will exist
+//      at a time. if compiling for a language with parallel semantics,
+//      the internal representation can shift to vecdeque<linkedlist<map<string, usize>>>
+//      e.g
+//      {
+//        int a = 9;
+//        int b = 10;
+//        c = a+b;
+//      }
+
+//      {
+//        int d = 11;
+//        int e = 12;
+//        f = d+e;
+//      }
+
+enum ScopeOp { Read, Update(Rc<dyn Instr>) }
 
 #[derive(Debug, Clone)]
-pub struct Scope { typ: TypeKind, inputs: Vec<Rc<Box<dyn Instr>>>, outputs: Vec<Rc<Box<dyn Instr>>>, pub nvs: VecDeque<HashMap<String, usize>> }
+pub struct Scope { typ: TypeKind, inputs: RefCell<Vec<Rc<dyn Instr>>>, pub outputs: RefCell<Vec<Weak<dyn Instr>>>, nvs: RefCell<VecDeque<HashMap<String, usize>>> }
 impl Scope {
-    pub fn new() -> Self { Scope { typ: TypeKind::Bot, inputs: vec![], outputs: vec![], nvs: VecDeque::new() } }
-    pub fn push_nv(&mut self) -> () { self.nvs.push_back(HashMap::new()); }
-    pub fn pop_nv(&mut self) -> () { self.nvs.pop_back(); }
+    pub fn new() -> Self { Scope { typ: TypeKind::Bot, inputs: RefCell::new(vec![]), outputs: RefCell::new(vec![]), nvs: RefCell::new(VecDeque::new()) } }
+    pub fn push_nv(self: Rc<Self>) -> () { self.nvs.borrow_mut().push_back(HashMap::new()); }
+    pub fn pop_nv(self: Rc<Self>) -> () { self.nvs.borrow_mut().pop_back(); }
+    pub fn read(self: Rc<Self>, alias: String) -> Rc<dyn Instr> { self.read_update(alias, ScopeOp::Read, self.nvs.borrow().len()-1) } // &mut self for shared foo. lazy phi.
+    pub fn update(self: Rc<Self>, alias: String, expr: Rc<dyn Instr>) -> Rc<dyn Instr> { self.read_update(alias, ScopeOp::Update(expr), self.nvs.borrow().len()-1)}
 
-    fn var_def(&mut self, alias: String, expr: Box<dyn Instr>) -> Result<&mut Self, FooError> {
-        let cur_nv = self.nvs.back_mut().ok_or(FooError::Unknown)?;
-        if cur_nv.contains_key(&alias) {
-            Err(FooError::DoubleDefine)
+    // shared read/update makes lazi phi creation easier TODO
+    fn read_update(self: &Rc<Self>, alias: String, op: ScopeOp, level: usize) -> Rc<dyn Instr> {
+        if level == 0 {
+            todo!()
+        }
+
+        let nvs = self.nvs.borrow();
+        let nv = nvs.get(level).unwrap();
+        if let Some(i) = nv.get(&alias) {
+            let expr = self.inputs.borrow()[*i].clone();
+            match op {
+                ScopeOp::Read => expr,
+                ScopeOp::Update(instr) => {
+                    self.inputs.borrow_mut()[*i] = instr; // updating std::vec calls drop on rc
+                    self.inputs.borrow()[*i].clone() // rc.clone()
+                },
+            }
         } else {
-            cur_nv.insert(alias, self.inputs.len());
-            // self.add_input(expr);
-            // expr.add_output(self);
-            Ok(self)
+            return self.read_update(alias, op, level-1);
         }
     }
 
-    fn var_apply(&self) -> Arc<dyn Instr> {
-        todo!()
+
+    fn write(self: &Rc<Self>, alias: String, expr: Rc<dyn Instr>) -> Result<&Rc<Self>, ScopeError> {
+        let mut nvs = self.nvs.borrow_mut();
+        let cur_nv = nvs.back_mut().ok_or(ScopeError::Unknown)?;
+        if cur_nv.contains_key(&alias) {
+            Err(ScopeError::DoubleDefine)
+        } else {
+            let foo = self.add_child(expr);
+            cur_nv.insert(alias, self.inputs.borrow().len());
+            Ok(self)
+        }
     }
 }
 
-// NB: the scope (a stack of nv) neither data nor control is still represented
-//     as an instruction within the graph to leverage def-use information in
-//     liveness analysis. the scope instruction has no outputs. that is, no
-//     instructions that use the scope instruction itself
-// impl Instr for Scope {
-//     fn kind(&self) -> InstrKind { InstrKind::Scope }
-// }
+impl Instr for Scope {
+    fn kind(&self) -> InstrKind { InstrKind::Scope }
+    fn inputs(&self) -> &RefCell<Vec<Rc<dyn Instr>>> { &self.inputs }
+    fn outputs(&self) -> &RefCell<Vec<Weak<dyn Instr>>> { &self.outputs }
+}
